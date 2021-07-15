3ab3a04cafef02331f221cededc9ea773fe53a55
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-07-15 11:35:47 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
-use serenity::model::channel::Message;
 use serenity::framework::standard::{
     StandardFramework,
     CommandResult,
