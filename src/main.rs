60b0e7e3b0e091247718edb072266dc58e68773b
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-07-27 22:27:07 -0700

    warning smh

--- src/main.rs
+++ src/main.rs
@@ -260,7 +260,7 @@ async fn invert(ctx: &Context, msg: &Message) -> CommandResult {
     
 #[command]
 async fn rainbow(ctx: &Context, msg: &Message) -> CommandResult {
-    manip_image(msg, ctx, apply_gradient).await;
+    manip_image(msg, ctx, apply_gradient).await?;
     
     Ok(())
 }
